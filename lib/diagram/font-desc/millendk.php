2126abc381c8339bfa11f04e17f662e8a0cf7ddf
Mihail Croitor <mcroitor@gmail.com>
2022-01-07 07:54:21 +0200

    Add files via upload

---
+++ lib/diagram/font-desc/millendk.php
@@ -0,0 +1,69 @@
+<?php
+$simbols["millendk"] = array();
+
+
+$simbols["millendk"]["8"] = "$";
+$simbols["millendk"]["7"] = "$";
+$simbols["millendk"]["6"] = "$";
+$simbols["millendk"]["5"] = "$";
+$simbols["millendk"]["4"] = "$";
+$simbols["millendk"]["3"] = "$";
+$simbols["millendk"]["2"] = "$";
+$simbols["millendk"]["1"] = "$";
+
+$simbols["millendk"]["|"] = "%";
+$simbols["millendk"]["top"] = "A\"\"\"\"\"\"\"\"S";
+$simbols["millendk"]["bottom"] = "D((((((((F"; //"7888888889";
+$simbols["millendk"]["/"] = "$";
+
+$simbols["millendk"]["tl"] = "A";
+$simbols["millendk"]["tt"] = "\"";
+$simbols["millendk"]["tr"] = "S";
+$simbols["millendk"]["bl"] = "D";
+$simbols["millendk"]["bb"] = "(";
+$simbols["millendk"]["br"] = "F";
+
+$simbols["millendk"]["-"] = " ";
+$simbols["millendk"]["+"] = "+";
+
+$simbols["millendk"]["a"] = "(";
+$simbols["millendk"]["b"] = "(";
+$simbols["millendk"]["c"] = "(";
+$simbols["millendk"]["d"] = "(";
+$simbols["millendk"]["e"] = "(";
+$simbols["millendk"]["f"] = "(";
+$simbols["millendk"]["g"] = "(";
+$simbols["millendk"]["h"] = "(";
+
+$simbols["millendk"]["K0"] = "k";
+$simbols["millendk"]["Q0"] = "q";
+$simbols["millendk"]["R0"] = "r";
+$simbols["millendk"]["B0"] = "b";
+$simbols["millendk"]["N0"] = "n";
+$simbols["millendk"]["P0"] = "p";
+
+$simbols["millendk"]["K1"] = "K";
+$simbols["millendk"]["Q1"] = "Q";
+$simbols["millendk"]["R1"] = "R";
+$simbols["millendk"]["B1"] = "B";
+$simbols["millendk"]["N1"] = "N";
+$simbols["millendk"]["P1"] = "P";
+
+$simbols["millendk"]["k0"] = "l";
+$simbols["millendk"]["q0"] = "w";
+$simbols["millendk"]["r0"] = "t";
+$simbols["millendk"]["b0"] = "v";
+$simbols["millendk"]["n0"] = "m";
+$simbols["millendk"]["p0"] = "o";
+
+$simbols["millendk"]["k1"] = "L";
+$simbols["millendk"]["q1"] = "W";
+$simbols["millendk"]["r1"] = "T";
+$simbols["millendk"]["b1"] = "W";
+$simbols["millendk"]["n1"] = "M";
+$simbols["millendk"]["p1"] = "O";
+
+$simbols["millendk"]["X0"] = 'x';
+$simbols["millendk"]["X1"] = 'X';
+
+$simbols["millendk"]["delta"] = 0;
---
+++ lib/diagram/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$fen = filter_input(INPUT_GET, "fen");
+if ($fen === false) {
+    exit("<b>using</b>:<br />index.php?fen=&lt;FEN&gt;");
+}
+
+
+$options = array(
+    "size" => strtolower(filter_input(INPUT_GET, 'size', FILTER_VALIDATE_INT)),
+    "style" => strtolower(filter_input(INPUT_GET, "style", FILTER_SANITIZE_FULL_SPECIAL_CHARS) ?? "alpha"),
+    "solid" => filter_input(INPUT_GET, "solid", FILTER_VALIDATE_BOOLEAN),
+    "color" => strtolower(filter_input(INPUT_GET, "color", FILTER_SANITIZE_FULL_SPECIAL_CHARS) ?? "black"),
+    "dbl_margin" => filter_input(INPUT_GET, "double", FILTER_VALIDATE_BOOLEAN)
+);
+
+require __DIR__ . '/diagram.class.php';
+
+$d = new Diagram($fen, $options);
+
+header('Content-type: image/png');
+imagepng($d->toImage());
+
