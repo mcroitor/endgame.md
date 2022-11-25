be4f02246a85398723fc8dad2040ceacb4647544
Mihail Croitor <mcroitor@gmail.com>
2022-11-25 09:55:09 +0200

    article template

--- theme/templates/article.template.php
+++ theme/templates/article.template.php
@@ -1,4 +1,7 @@
-<article>
-    <title><!-- title --></title>
-    <body><!-- body --></body>
-</article>
+<div class="article">
+    <div class="article-title">
+        <h3><!-- title --></h3>
+        <h4><!-- published --></h4>
+    </div>
+    <div class="article-body"><!-- body --></div>
+</div>
